13f8fd99c128b4cd5373d146605a5e282db5cc24
Jagdeep gill [jatt life thuglife] <64634071+patwari6862@users.noreply.github.com>
2021-01-30 10:27:39 +0530

    Update rename_video.py

--- plugins/rename_video.py
+++ plugins/rename_video.py
@@ -79,3 +79,97 @@ async def rename_video(bot, message):
         file_name = message.text
         description = script.CUSTOM_CAPTION_UL_FILE.format(newname=file_name)
         download_location = Config.DOWNLOAD_LOCATION + "/"
+        
+        a = await bot.send_message(
+            chat_id=message.chat.id,
+            text=script.DOWNLOAD_START,
+            reply_to_message_id=message.message_id
+        )
+        
+        c_time = time.time()
+        the_real_download_location = await bot.download_media(
+            message=media,
+            file_name=download_location,
+            progress=progress_for_pyrogram,
+            progress_args=(
+                script.DOWNLOAD_START,
+                a,
+                c_time
+            )
+        )
+        if the_real_download_location is not None:
+            await bot.edit_message_text(
+                text=script.SAVED_RECVD_DOC_FILE,
+                chat_id=message.chat.id,
+                message_id=a.message_id
+            )
+
+            new_file_name = download_location + file_name + "." + extension
+            os.rename(the_real_download_location, new_file_name)
+            await bot.edit_message_text(
+                text=script.UPLOAD_START,
+                chat_id=message.chat.id,
+                message_id=a.message_id
+                )
+            # logger.info(the_real_download_location)
+
+            thumb_image_path = download_location + str(message.from_user.id) + ".jpg"
+            if not os.path.exists(thumb_image_path):
+                mes = await thumb(message.from_user.id)
+                if mes != None:
+                    m = await bot.get_messages(message.chat.id, mes.msg_id)
+                    await m.download(file_name=thumb_image_path)
+                    thumb_image_path = thumb_image_path
+                else:
+                    thumb_image_path = None                    
+            else:
+                width = 0
+                height = 0
+                metadata = extractMetadata(createParser(thumb_image_path))
+                if metadata.has("width"):
+                    width = metadata.get("width")
+                if metadata.has("height"):
+                    height = metadata.get("height")
+                Image.open(thumb_image_path).convert("RGB").save(thumb_image_path)
+                img = Image.open(thumb_image_path)
+                img.resize((320, height))
+                img.save(thumb_image_path, "JPEG")
+
+            c_time = time.time()
+            await bot.send_document(
+                chat_id=message.chat.id,
+                document=new_file_name,
+                thumb=thumb_image_path,
+                caption=description,
+                # reply_markup=reply_markup,
+                reply_to_message_id=message.reply_to_message.message_id,
+                progress=progress_for_pyrogram,
+                progress_args=(
+                    script.UPLOAD_START,
+                    a, 
+                    c_time
+                )
+            )
+
+            try:
+                os.remove(new_file_name)
+            except:
+                pass                 
+            try:
+                os.remove(thumb_image_path)
+            except:
+                pass  
+
+            await bot.edit_message_text(
+                text=script.AFTER_SUCCESSFUL_UPLOAD_MSG,
+                chat_id=message.chat.id,
+                message_id=a.message_id,
+                disable_web_page_preview=True
+            )
+            
+    else:
+        await bot.send_message(
+            chat_id=message.chat.id,
+            text="You're B A N N E D",
+            reply_to_message_id=message.message_id
+        )
